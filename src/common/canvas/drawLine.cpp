38cc330496b81fd37b32059b11ff1b0a58cdd3bc
h8m0ss <danarmenta32@gmail.com>
2024-01-24 12:26:28 -0600

    FIX: Default argument of

--- src/common/canvas/drawLine.cpp
+++ src/common/canvas/drawLine.cpp
@@ -1,7 +1,6 @@
 #include "drawLine.hpp"
 
-
-void drawLine(rgb_matrix::Canvas *canvas, int x, int y, int length, Color c, bool horizontal=true) {
+void drawLine(rgb_matrix::Canvas *canvas, int x, int y, int length, Color c, bool horizontal) {
   bool vertical{!horizontal};
   for (int i{}; i<length; i++) 
     c.setCanvasPixel(x+i*horizontal, y+i*vertical, canvas);
