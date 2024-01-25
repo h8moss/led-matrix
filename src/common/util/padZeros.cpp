11c1aa3d44ffa11afb87dcb5bca6307ae4fbb1bd
h8m0ss <danarmenta32@gmail.com>
2024-01-25 12:05:23 -0600

    FEAT: Added padZeros funciton

---
+++ src/common/util/padZeros.cpp
@@ -0,0 +1,10 @@
+#include "pad_zeros.hpp"
+#include <string>
+
+std::string padZeros(int number, int length) {
+	std::string numStr{std::to_string(number)};
+	while (numStr.length() < length) 
+		numStr = '0' + numStr;
+
+	return numStr;
+}
---
+++ src/common/util/padZeros.hpp
@@ -0,0 +1,5 @@
+#pragma once
+
+#include <string>
+
+std::string padZeros(int number, int length=2);
