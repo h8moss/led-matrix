bbdf4b0fcf81404844791294219b323131a2b0db
h8moss <danarmenta32@gmail.com>
2024-03-13 22:01:56 -0600

    REFAC: Removed unused files

--- src/common/canvas/drawLine.cpp
+++
@@ -1,7 +0,0 @@
-#include "common/canvas/drawLine.hpp"
-
-void drawLine(rgb_matrix::Canvas *canvas, int x, int y, int length, Color c, bool horizontal) {
-  bool vertical{!horizontal};
-  for (int i{}; i<length; i++) 
-    c.setCanvasPixel(x+i*horizontal, y+i*vertical, canvas);
-}
