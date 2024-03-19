6143e5cb05d4439f0a6a14a75b2f7ab4dc6af45e
h8moss <danarmenta32@gmail.com>
2024-03-19 17:31:52 -0600

    REFAC: Fixed imports

--- src/modules/colors/renderers/circle_animation_renderer.cpp
+++ src/modules/colors/renderers/circle_animation_renderer.cpp
@@ -3,20 +3,14 @@
 
 #include <cmath>
 
-Colors::CircleAnimationRenderer::CircleAnimationRenderer(BetterCanvas *canvas, bool _shrink = false)
-    : radius{},
-      currentRadius{},
-      centerX{},
-      centerY{},
-      color{},
-      loopCount{},
-      shrink{_shrink},
-      Module(canvas)
-{
-}
+Colors::CircleAnimationRenderer::CircleAnimationRenderer(BetterCanvas *canvas,
+                                                         bool _shrink)
+    : radius{}, currentRadius{}, centerX{}, centerY{}, color{}, loopCount{},
+      shrink{_shrink}, Module(canvas) {}
 
-void Colors::CircleAnimationRenderer::setup()
-{
+Colors::CircleAnimationRenderer::~CircleAnimationRenderer() {}
+
+void Colors::CircleAnimationRenderer::setup() {
   canvas->clear();
   radius = (int)std::sqrt(canvas->getHeight() * canvas->getHeight() +
                           canvas->getWidth() * canvas->getWidth()) +
@@ -30,49 +24,38 @@ void Colors::CircleAnimationRenderer::setup()
   color = configuration.getColor();
 }
 
-int Colors::CircleAnimationRenderer::render()
-{
-  int animationProgress{
-      loopCount %
-      (int)(configuration.animationDuration + configuration.duration)};
-  if (animationProgress == 0)
-  {
+int Colors::CircleAnimationRenderer::render() {
+  int animationProgress{loopCount % (int)(configuration.animationDuration +
+                                          configuration.duration)};
+  if (animationProgress == 0) {
     canvas->fill(color);
     color = configuration.getColor();
   }
 
-  if (animationProgress >= configuration.duration)
-  {
-    float percent{
-        (((float)animationProgress - configuration.duration) / configuration.animationDuration)};
+  if (animationProgress >= configuration.duration) {
+    float percent{(((float)animationProgress - configuration.duration) /
+                   configuration.animationDuration)};
     if (this->shrink)
       percent = 1.0f - percent;
     int nextRadius{(int)(radius * percent)};
-    if (nextRadius != currentRadius)
-    {
+    if (nextRadius != currentRadius) {
       currentRadius = nextRadius;
-      for (int x{}; x < canvas->getWidth(); x++)
-      {
-        for (int y{}; y < canvas->getHeight(); y++)
-        {
+      for (int x{}; x < canvas->getWidth(); x++) {
+        for (int y{}; y < canvas->getHeight(); y++) {
           int xCentered{x - centerX};
           int yCentered{y - centerY};
           int distanceSquared{xCentered * xCentered + yCentered * yCentered};
           int squaredRadius{currentRadius * currentRadius};
-          if (distanceSquared > squaredRadius && this->shrink || distanceSquared < squaredRadius && !this->shrink)
-          {
+          if ((distanceSquared > squaredRadius && this->shrink) ||
+              (distanceSquared < squaredRadius && !this->shrink)) {
             canvas->setPixel(x, y, color);
           }
         }
       }
     }
-
-    ++loopCount;
-    return 1000;
   }
+  ++loopCount;
+  return 1000;
 }
 
-void Colors::CircleAnimationRenderer::teardown()
-{
-  canvas->clear();
-}
+void Colors::CircleAnimationRenderer::teardown() { canvas->clear(); }
--- src/modules/colors/renderers/pulse_animation_renderer.cpp
+++ src/modules/colors/renderers/pulse_animation_renderer.cpp
@@ -1,58 +1,44 @@
 #include "modules/colors/renderers/pulse_animation_renderer.hpp"
 
 Colors::PulseAnimationRenderer::PulseAnimationRenderer(BetterCanvas *canvas)
-    : currentColor{},
-      animationSplits{},
-      loopCount{},
-      Module(canvas)
-{
+    : currentColor{}, animationSplits{}, loopCount{}, Module(canvas) {}
+
+void Colors::PulseAnimationRenderer::setup() {
+  canvas->clear();
+  currentColor = configuration.getColor();
+
+  animationSplits[0] = (int)(configuration.animationDuration / 2.0f);
+  animationSplits[1] =
+      (int)((configuration.animationDuration + configuration.duration) / 2.0f);
+  animationSplits[2] =
+      (int)(configuration.animationDuration / 2.0f + configuration.duration);
+  animationSplits[3] =
+      (int)(configuration.animationDuration + configuration.duration);
+
+  loopCount = 0;
 }
 
-void Colors::PulseAnimationRenderer::setup()
-{
-    canvas->clear();
-    currentColor = configuration.getColor();
+int Colors::PulseAnimationRenderer::render() {
+  int animationProgress = loopCount % (int)animationSplits[3];
+  if (animationProgress < animationSplits[0]) {
+    // animate color in
+    float progress{(float)animationProgress / animationSplits[0]};
+    canvas->fill(currentColor * progress);
+  } else if (animationProgress < animationSplits[1]) {
+  } else if (animationProgress < animationSplits[2]) {
+    float progress{(float)(-animationProgress + animationSplits[2]) /
+                   (float)(animationSplits[2] - animationSplits[1])};
+    canvas->fill(currentColor * progress);
+  } else if (animationProgress < animationSplits[3]) {
+    // stay black
+    if (animationProgress - animationSplits[3] - 1 < 1) {
+      currentColor = configuration.getColor(currentColor);
+    }
+  }
 
-    animationSplits[0] = (int)(configuration.animationDuration / 2.0f);
-    animationSplits[1] = (int)((configuration.animationDuration + configuration.duration) / 2.0f);
-    animationSplits[2] = (int)(configuration.animationDuration / 2.0f + configuration.duration);
-    animationSplits[3] = (int)(configuration.animationDuration + configuration.duration);
+  ++loopCount;
 
-    loopCount = 0;
+  return 1000;
 }
 
-int Colors::PulseAnimationRenderer::render()
-{
-    int animationProgress = loopCount % (int)animationSplits[3];
-    if (animationProgress < animationSplits[0])
-    {
-        // animate color in
-        float progress{(float)animationProgress / animationSplits[0]};
-        canvas->fill(currentColor * progress);
-    }
-    else if (animationProgress < animationSplits[1])
-    {
-    }
-    else if (animationProgress < animationSplits[2])
-    {
-        float progress{(float)(-animationProgress + animationSplits[2]) / (float)(animationSplits[2] - animationSplits[1])};
-        canvas->fill(currentColor * progress);
-    }
-    else if (animationProgress < animationSplits[3])
-    {
-        // stay black
-        if (animationProgress - animationSplits[3] - 1 < 1)
-        {
-            currentColor = configuration.getColor(currentColor);
-        }
-    }
-
-    ++loopCount;
-
-    return 1000;
-}
-
-void Colors::PulseAnimationRenderer::teardown()
-{
-    canvas->clear();
-}
+void Colors::PulseAnimationRenderer::teardown() { canvas->clear(); }
