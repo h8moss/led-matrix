91e09a257983a5ff51577dfd9e11b586ce47463e
h8moss <danarmenta32@gmail.com>
2024-03-19 17:21:52 -0600

    BUILD: Fixed linking order and removed unused source

--- include/modules/module.hpp
+++ include/modules/module.hpp
@@ -3,21 +3,20 @@
 
 #include <string>
 
-template <typename TConfig>
-class Module {
+template <typename TConfig> class Module {
 public:
-    Module(BetterCanvas* canvas);
+  Module(BetterCanvas *_canvas) : configuration{}, canvas{_canvas} {}
 
-    std::string name;
-    std::string description;
+  std::string name;
+  std::string description;
 
-    virtual void setup() = 0;
-    virtual int render() = 0;
-    virtual void teardown() = 0;
-    TConfig configuration;
+  virtual void setup() = 0;
+  virtual int render() = 0;
+  virtual void teardown() = 0;
+  TConfig configuration;
 
-    ~Module();
+  virtual ~Module() { delete canvas; }
 
 protected:
-    BetterCanvas* canvas;
+  BetterCanvas *canvas = nullptr;
 };
