1e61a4ef286bb3f5a2c7c9eaa297a41f14140174
h8moss <danarmenta32@gmail.com>
2024-03-20 03:49:38 -0600

    FEAT: Added basic time date module

---
+++ include/modules/time-date/time_date_module.hpp
@@ -0,0 +1,24 @@
+#pragma once
+#include "common/util/better_canvas.hpp"
+#include "modules/module.hpp"
+#include "modules/time-date/time_date_configuration.hpp"
+
+#include <string>
+
+namespace TimeDate {
+class TimeDateModule : public Module<Configuration> {
+public:
+  TimeDateModule(BetterCanvas *_canvas);
+
+  virtual void setup() override;
+  virtual int render() override;
+  virtual void teardown() override;
+
+  virtual ~TimeDateModule();
+
+protected:
+  std::string hours;
+  std::string minutes;
+  std::string seconds;
+};
+} // namespace TimeDate
---
+++ src/modules/time-date/time_date_module.cpp
@@ -0,0 +1,5 @@
+#include "modules/time-date/time_date_module.hpp"
+#include "common/util/better_canvas.hpp"
+
+TimeDate::TimeDateModule::TimeDateModule(BetterCanvas *_canvas)
+    : Module(_canvas) {}
