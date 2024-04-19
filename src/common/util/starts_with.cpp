d51474daed3560de3e124eaa9eaadfbd124b0c4a
h8moss <danarmenta32@gmail.com>
2024-04-19 13:07:04 -0600

    FIX: Fixed startsWith not properly checking

--- src/common/util/starts_with.cpp
+++ src/common/util/starts_with.cpp
@@ -1,10 +1,12 @@
 #include "common/util/starts_with.hpp"
+#include "common/util/debug_log.hpp"
 
 bool startsWith(std::string string, std::string pattern) {
+  dLog("Comparing " + string + " to " + pattern);
   if (pattern.size() > string.size())
     return false;
 
-  for (size_t i{}; i < string.size(); ++i) {
+  for (size_t i{}; i < pattern.size(); ++i) {
     if (pattern[i] != string[i]) {
       return false;
     }
