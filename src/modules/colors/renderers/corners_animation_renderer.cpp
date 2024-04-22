61c5c8c9a800e75d4e2e7e0ae216e598484d9d6a
h8moss <danarmenta32@gmail.com>
2024-04-21 22:48:14 -0600

    FEAT: Added runOnce?

--- src/modules/colors/renderers/corners_animation_renderer.cpp
+++ src/modules/colors/renderers/corners_animation_renderer.cpp
@@ -20,51 +20,15 @@ void Colors::CornersAnimationRenderer::setup() {
 }
 
 long int Colors::CornersAnimationRenderer::render() {
-  // int animationProgress =
-  //     loopCount %
-  //     ((int)(getConfig()->animationDuration + getConfig()->duration));
-
-  // if (getConfig()->runOnce &&
-  //     animationProgress ==
-  //         ((int)(getConfig()->animationDuration + getConfig()->duration)) -
-  //         1) {
-  //   return -1;
-  // }
-
-  // if (animationProgress == 0) {
-  //   if (!getConfig()->fading)
-  //     canvas->fill(currentColor);
-  //   currentColor = getConfig()->getColor();
-  // }
-
-  // if ((float)animationProgress >= getConfig()->duration) {
-  //   int delta{static_cast<int>(std::floor(
-  //       totalDiagonals * ((float)animationProgress - getConfig()->duration) /
-  //       (getConfig()->animationDuration)))};
-  //   for (int x{}; x <= delta; x++) {
-  //     canvas->setPixel(x, delta - x - 1, currentColor);
-  //   }
-  // }
-
-  // ++loopCount;
-  // return 1000;
-
-  // States: 0. When is black, 1. When is coloring, 2. When is black again
-  // States: 0. When is full of color A, 2. When is coloring with B, 3. When is
-  // full of color B
-
-  // duration and animation duration. Duration is state 0, animation duration is
-  // state 1 is animadtionDur and state 2 last 0 secs
-
   int progress{loopCount % (int)(fullDuration)};
 
   if (progress == 0) {
     canvas->fill(getConfig()->fading ? Color::black : currentColor);
     currentColor = getConfig()->getColor();
-  } else if (progress < getConfig()->animationDuration) {
+  } else if ((float)progress < getConfig()->animationDuration) {
 
     for (int iter{}; iter < (getConfig()->fading ? 11 : 1); iter++) {
-      float fade{1.0f - (0.1f * iter)}; // BUG: If we ever make `iter` this an
+      float fade{1.0f - (0.1f * iter)}; // TODO: If we ever make `iter` this an
                                         // argument, edit 0.1f to 1/iter
       int delta{static_cast<int>(std::floor(totalDiagonals * (float)progress /
                                             getConfig()->animationDuration)) -
@@ -74,12 +38,10 @@ long int Colors::CornersAnimationRenderer::render() {
         canvas->setPixel(x, delta - x - 1, currentColor * fade);
       }
     }
-  } else if (progress < (int)(fullDuration)) {
-
-    // state 1
-  } else {
   }
-
+  if (getConfig()->runOnce && progress == (int)fullDuration - 1) {
+    return -1;
+  }
   ++loopCount;
   return 1000;
 }
