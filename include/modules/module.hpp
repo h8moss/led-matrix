d0ee903faa0494e28d8d22424f912b4de3baeba2
h8moss <danarmenta32@gmail.com>
2024-05-16 21:07:28 -0600

    FIX: Module implementation

--- include/modules/module.hpp
+++ include/modules/module.hpp
@@ -1,30 +1,27 @@
 #pragma once
 #include "common/util/better_canvas.hpp"
-#include "modules/module_configuration.hpp"
+
+#include "CLI/CLI.hpp"
 
 #include <string>
 
 class Module {
 public:
-  Module(BetterCanvas *_canvas, ModuleConfiguration *config)
-      : configuration{config}, canvas{_canvas} {}
+  Module(BetterCanvas *_canvas) : canvas{_canvas} {}
 
   std::string name;
-  std::string description; // TODO: What is this even for??? REMOVE IT
+  std::string description;
 
   virtual void setup() = 0;
   virtual long int render() = 0;
   virtual void teardown() = 0;
-  ModuleConfiguration *configuration;
 
-  virtual void createConfiguration() = 0;
+  virtual void addFlags(CLI::App *app) = 0;
 
   virtual ~Module() {
     delete canvas;
-    delete configuration;
 
     canvas = nullptr;
-    configuration = nullptr;
   }
 
 protected:
