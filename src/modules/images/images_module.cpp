ee132fd4b8bc05e6675bda554dbbf4c82b98124e
h8moss <danarmenta32@gmail.com>
2024-05-22 21:06:14 -0600

    FEAT: Implemented ImagesModule::addFlags

--- src/modules/images/images_module.cpp
+++ src/modules/images/images_module.cpp
@@ -33,7 +33,31 @@ long int Images::ImagesModule::render() {
 
 void Images::ImagesModule::teardown() { canvas->clear(); }
 
-void Images::ImagesModule::addFlags(CLI::App *app) {}
+void Images::ImagesModule::addFlags(CLI::App *app) {
+  auto cmd{app->add_subcommand(this->name, this->description)};
+
+  cmd->add_option("--image,-i", config.images,
+                  "An image to showcase, can be passed more than once");
+  cmd->add_option("--duration,-d", config.durations,
+                  "The time the image should be on screen in ms. If passed "
+                  "more than once, matches the correspoinding image");
+  cmd->add_option("--fit", config.fit, "How to fit the image into the matrix")
+      ->transform(CLI::CheckedTransformer(Images::Configuration::imageFitMap,
+                                          CLI::ignore_case));
+  cmd->add_option("--x-align,-x", config.xAlignment,
+                  "The alignment of the image on the x axis")
+      ->transform(CLI::CheckedTransformer(Images::Configuration::alignmentMap,
+                                          CLI::ignore_case));
+  cmd->add_option("--y-align,-y", config.yAlignment,
+                  "The alignment of the image on the y axis")
+      ->transform(CLI::CheckedTransformer(Images::Configuration::alignmentMap,
+                                          CLI::ignore_case));
+  cmd->add_flag("--no-loop", config.exitOnEnd,
+                "If passed, the images will not loop and instead the program "
+                "will exit after once run");
+  cmd->add_flag("--shuffle,-s", config.shuffleImages,
+                "Shuffle the images before displaying them");
+}
 
 void Images::ImagesModule::readArguments(
     std::map<std::string, std::vector<std::string>> args) {}
