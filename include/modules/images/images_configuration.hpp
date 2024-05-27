5c19b6b63d6a9b030d4c72e100449107b6987e43
h8moss <danarmenta32@gmail.com>
2024-05-27 12:37:30 -0600

    FIX: Added missing imagefit

--- include/modules/images/images_configuration.hpp
+++ include/modules/images/images_configuration.hpp
@@ -6,7 +6,13 @@
 
 namespace Images {
 
-enum ImageFit { stretch, crop, box };
+enum ImageFit {
+  stretch, // Ignores aspect ratio, stretches to fit
+  crop, // Grows the image until it fills the screen, even if it crops part of
+        // it
+  box,  // Grows the image until it fits the screen, not cropping anything
+  place // Places the image in it's original dimensions, does not resize at all
+};
 enum Alignment { leading, trailing, center };
 
 struct Configuration {
