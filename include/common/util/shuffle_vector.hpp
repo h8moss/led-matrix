d1f60f43f7e09acd14e0a8bb4d3c5eb307713af0
h8moss <danarmenta32@gmail.com>
2024-08-09 10:03:12 -0600

    FIX: Moved definition of shuffle into header file

--- include/common/util/shuffle_vector.hpp
+++ include/common/util/shuffle_vector.hpp
@@ -2,4 +2,15 @@
 
 #include <vector>
 
-template <typename T> std::vector<T> shuffle(std::vector<T> vector);
+// template <typename T> std::vector<T> shuffle(std::vector<T> vector);
+
+template <typename T>
+std::vector<T> shuffle(std::vector<T> v)
+{
+  std::random_device rd;
+  std::mt19937 g(rd());
+
+  std::shuffle(v.begin(), v.end(), g);
+
+  return v;
+}
--- src/common/util/shuffle_vector.cpp
+++ src/common/util/shuffle_vector.cpp
@@ -4,11 +4,11 @@
 #include <cstdlib>
 #include <random>
 
-template <typename T> std::vector<T> shuffle(std::vector<T> v) {
-  std::random_device rd;
-  std::mt19937 g(rd());
-
-  std::shuffle(v.begin(), v.end(), g);
-
-  return v;
-}
+// template <typename T> std::vector<T> shuffle(std::vector<T> v) {
+//   std::random_device rd;
+//   std::mt19937 g(rd());
+//
+//   std::shuffle(v.begin(), v.end(), g);
+//
+//   return v;
+// }
