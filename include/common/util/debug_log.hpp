10d8c810633d8eb9cd4036c8db39246ba4849089
h8moss <danarmenta32@gmail.com>
2025-01-15 23:13:08 -0600

    Added date-time to logging

--- include/common/util/debug_log.hpp
+++ include/common/util/debug_log.hpp
@@ -1,11 +1,22 @@
 #pragma once
 
+#include <string>
 #ifdef DEBUG
+#include <chrono>
+#include <ctime>
 #include <iostream>
 #endif
 
 template <typename T> void dLog(const T str, char endln = '\n') {
 #ifdef DEBUG
-  std::cout << str << endln;
+  std::time_t t = std::time(nullptr);
+  std::tm *now = std::localtime(&t);
+  std::string finalTime{
+      std::to_string(now->tm_mday) + "/" + std::to_string(now->tm_mon) + "/" +
+      std::to_string(now->tm_year) + " - " + std::to_string(now->tm_hour) +
+      ":" + std::to_string(now->tm_min) + ":" + std::to_string(now->tm_sec) +
+      "."};
+
+  std::cout << "(" << finalTime << ") " << str << endln;
 #endif
 }
